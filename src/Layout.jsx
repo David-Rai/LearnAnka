83b2b03c5a248ae41f6ebe5920e858b5e969e59d
david-rai <davidkhaling123@gmail.com>
2026-03-30 01:02:58 +0545

    Fix:Overflowing content not showing

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 const Layout = () => {
   return (
-    <main className="w-full h-dvh overflow-hidden ">
+    <main className="w-full h-dvh">
       <Header />
 
       {/* other main content here */}
